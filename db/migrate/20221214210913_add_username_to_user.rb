cdf53ddb571c8b9c9c3cc0ae1580f8cd76029616
Timothy Carey <timcarey1989@gmail.com>
2022-12-14 16:37:04 -0500

    added uniqueness validation to usernames

--- db/migrate/20221214210913_add_username_to_user.rb
+++ db/migrate/20221214210913_add_username_to_user.rb
@@ -1,5 +1,5 @@
 class AddUsernameToUser < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :username, :string, null: false
+    add_column :users, :username, :string, null: false, unique: true
   end
 end
