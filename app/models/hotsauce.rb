8b56ab9d5536fb54ec9ae8b3cde4334b8cd681f6
Timothy Carey <timcarey1989@gmail.com>
2022-12-14 14:59:36 -0500

    UI improvements

--- app/models/hotsauce.rb
+++ app/models/hotsauce.rb
@@ -2,5 +2,7 @@ class Hotsauce < ApplicationRecord
   validates :rating, :inclusion => 1..5, presence: true
   validates :name, :description, :review, :location, presence: true
 
-  has_one_attached :sauce_bottle_image
+  has_one_attached :sauce_bottle_image do |attachable|
+    attachable.variant :card, resize_to_limit: [ 300, 400 ]
+  end
 end
