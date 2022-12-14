0cb3317cfebd6dc771927a583edbdf9d372a1259
Timothy Carey <timcarey1989@gmail.com>
2022-12-14 16:29:39 -0500

    authenticate with username and updated navbar to sign in/out

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_14_203536) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_14_210913) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_14_203536) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
