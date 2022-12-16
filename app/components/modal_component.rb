c02cdef364d11fe3eed582b1c3fab884b3247a7a
Timothy Carey <timcarey1989@gmail.com>
2022-12-16 12:40:18 -0500

    added dom_id's to modals for unique id's to prevent potential bugs

--- app/components/modal_component.rb
+++ app/components/modal_component.rb
@@ -1,4 +1,6 @@
 class ModalComponent < ViewComponent::Base
+  attr_reader :resource
+
   def initialize(modal_title:, primary_button_name:, modal_body:, btn_style:, btn_method:, resource:)
     @modal_title = modal_title
     @primary_button_name = primary_button_name
