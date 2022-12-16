ad13448fd8dd5bb51ad8689d966818d260053300
Dave Paola <dpaola2@gmail.com>
2022-12-16 09:18:50 -0800

    conditionally render nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  helper_method :turbo_frame_request?
+  helper_method :turbo_native_app?
+
   protected
 
   def configure_permitted_parameters
