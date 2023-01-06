775694be4d45d6b3c02c141ea39f86eba2f7eae1
Timothy Carey <timcarey1989@gmail.com>
2023-01-06 15:11:59 -0500

    category column on hotsauce model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_14_210913) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_06_192228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_14_210913) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category", null: false
   end
 
   create_table "users", force: :cascade do |t|
