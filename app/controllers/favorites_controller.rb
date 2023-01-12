9dfb0c3574caf0edc20bb8b3819aaf42fa53f916
Dave Paola <dpaola2@gmail.com>
2023-01-12 11:14:10 -0800

    adding/removing favorites from hotsauce card works

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,38 @@
+class FavoritesController < ApplicationController
+  def index
+  end
+
+  def create
+    find_hotsauce(key: :hotsauce_id)
+    @favorite = Favorite.new(user: current_user, hotsauce: @hotsauce)
+    
+    if @favorite.save
+      flash[:success] = "Favorited #{@hotsauce.name}"
+    else
+      flash[:error] = "Something went wrong."
+    end
+    redirect_to hotsauce_path(@hotsauce)
+  end
+
+  def destroy
+    find_favorite
+    @hotsauce = @favorite.hotsauce
+
+    if @favorite.destroy
+      flash[:success] = "Removed #{@hotsauce.name} from favorites."
+    else
+      flash[:error] = "Something went wrong."
+    end
+    redirect_to hotsauce_path(@hotsauce)
+  end
+
+  private
+
+  def find_hotsauce(key: :id)
+    @hotsauce = Hotsauce.find(params[key])
+  end
+
+  def find_favorite(key: :id)
+    @favorite = Favorite.find(params[key])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :favorites, only: [:index, :destroy, :create]
   resources :hotsauces
   root "hotsauces#index"
 end
