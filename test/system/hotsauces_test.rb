58ee615f2b2607d3f7ccdb4ea3192d413d64ba0a
Dave Paola <dpaola2@gmail.com>
2023-01-12 13:06:15 -0800

    fix the system tests

--- test/system/hotsauces_test.rb
+++ test/system/hotsauces_test.rb
@@ -3,11 +3,12 @@ require "application_system_test_case"
 class HotsaucesTest < ApplicationSystemTestCase
   setup do
     @hotsauce = hotsauces(:one)
+    sign_in users(:tim)
   end
 
   test "visiting the index" do
     visit hotsauces_url
-    assert_selector "h1", text: "Hot Sauces"
+    assert_selector "h4", text: "Hot Sauces"
   end
 
   test "should create hotsauce" do
@@ -15,14 +16,10 @@ class HotsaucesTest < ApplicationSystemTestCase
     click_on "New hotsauce"
 
     fill_in "Description", with: @hotsauce.description
-    fill_in "Location", with: @hotsauce.location
     fill_in "Name", with: @hotsauce.name
-    select "4", from: "hotsauce_rating"
-    fill_in "Review", with: @hotsauce.review
     click_on "Create Hotsauce"
 
     assert_text "Hotsauce was successfully created"
-    click_on "Back"
   end
 
   test "should update Hotsauce" do
@@ -30,13 +27,9 @@ class HotsaucesTest < ApplicationSystemTestCase
     click_on "Edit this hotsauce", match: :first
 
     fill_in "Description", with: @hotsauce.description
-    fill_in "Location", with: @hotsauce.location
     fill_in "Name", with: @hotsauce.name
-    select "2", from: "hotsauce_rating"
-    fill_in "Review", with: @hotsauce.review
     click_on "Update Hotsauce"
 
     assert_text "Hotsauce was successfully updated"
-    click_on "Back"
   end
 end
