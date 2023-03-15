51093a4e7a0c26a08eba00e34f0c7299bf039559
timcarey <timcarey1989@gmail.com>
2023-03-15 00:37:43 -0400

    new root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
@@ -14,7 +15,7 @@ Rails.application.routes.draw do
     resource :like, module: :checkins
   end
   
-  root "feed_items#index"
+  root "home#index"
 
   get "/users/:id/profile", to: "users/profiles#index", as: "user_profile"
 end
