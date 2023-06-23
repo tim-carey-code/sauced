1c1ebaf2bcf1bf322ab0192e5b46a8979c9e6c99
timcarey <timcarey1989@gmail.com>
2023-06-22 23:21:48 -0400

    remove route

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
-  resources :favorites, only: [:index, :destroy, :create]
+  resources :favorites, only: %i[index destroy create]
   resources :feed_items, only: [:index]
   resources :hotsauces do
     member do
-      resources :checkins, only: [:new, :create]
+      resources :checkins, only: %i[new create]
     end
   end
 
   resources :checkins do
     resource :like, module: :checkins
   end
-  
-  root "home#index"
 
-  get "/users/:id/profile", to: "users/profiles#index", as: "user_profile"
+  root 'home#index'
+
+  get '/users/:id/profile', to: 'users/profiles#index', as: 'user_profile'
 end
