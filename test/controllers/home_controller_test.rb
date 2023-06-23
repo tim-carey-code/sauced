3a4fac815682edc9aa13a740477ff81e98648367
timcarey <timcarey1989@gmail.com>
2023-06-23 16:27:16 -0400

    fix failing test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
-    get root_url
-    assert_response :success
+  include Devise::Test::IntegrationHelpers
+
+  test "should redirect user to feed items if signed in" do
+    @user = users(:tim)
+    sign_in @user
+
+    get root_path
+    assert_redirected_to feed_items_path
+  end
+
+  test "should redirect user to sign in if not signed in" do
+    get root_path
+    assert_redirected_to new_user_session_path
   end
 end
