45bb13c022cb306e800ed68893388a935bb1dce1
timcarey <timcarey1989@gmail.com>
2023-07-12 20:56:04 -0400

    fix sign up page + fix hotsauce partial + border on show page + change devise shared links

--- app/components/card_component.rb
+++ app/components/card_component.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class CardComponent < ViewComponent::Base
-  def initialize(container_classes: 'w-96', img_src: nil, img_alt: nil, card_body_classes: nil)
+  def initialize(container_classes: 'w-96', img_src: nil, img_alt: nil, card_body_classes: nil, title: nil)
     super()
     @img_src = img_src
     @img_alt = img_alt
     @container_classes = container_classes
     @card_body_classes = card_body_classes
+    @title = title
   end
 end
